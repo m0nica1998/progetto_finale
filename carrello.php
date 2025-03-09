9dd24dc94c32076f87d7dc9a6fa4199aa09b6ce3
m0nica1998 <monica.cesario98@icloud.com>
2025-03-09 15:43:15 +0100

    add form for modify quantity

--- carrello.php
+++ carrello.php
@@ -55,7 +55,27 @@ include 'header.php' ?>
                         </td>
                         <td><img src="imgs/<?php echo $_SESSION["carrello"][$i]['immagine'] ?>" alt="<?php echo $_SESSION["carrello"][$i]['nome'] ?>" width="50" height="50"></td>
                         <td><?php echo $_SESSION["carrello"][$i]['nome'] ?></td>
-                        <td><?php echo $_SESSION["carrello"][$i]['quantita'] ?></td>
+                        <td><?php echo $_SESSION["carrello"][$i]['quantita'] ?>
+                        <form action="controller_carrello.php?action=edit&method=up&id=<?php echo $_SESSION["carrello"][$i]['id']?>" method="post">
+                            <button
+                                type="submit"
+                                class="btn btn-primary"
+                            >
+                                up
+                            </button>
+                            
+                        </form>
+                        <form action="controller_carrello.php?action=edit&method=down&id=<?php echo $_SESSION["carrello"][$i]['id']?>" method="post">
+                            <button
+                                type="submit"
+                                class="btn btn-primary"
+                            >
+                                down
+                            </button>
+                            
+                        </form>
+                    
+                    </td>
                         <td><?php echo $_SESSION["carrello"][$i]['prezzo'] ?> €</td>
                     </tr>
                 </tbody>
