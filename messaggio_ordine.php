a77fed880c569d61130e73bde8c48cd075cde6ad
m0nica1998 <monica.cesario98@icloud.com>
2025-03-16 09:30:19 +0100

    add message order complete

---
+++ messaggio_ordine.php
@@ -0,0 +1,10 @@
+<?php 
+session_start();
+$title = "Messaggio ordine";
+include("header.php") ?>
+
+<main class="main-mess-ordine">
+<h3 class="text-center">Complimenti! Hai completato con successo un ordine</h3>
+</main>
+
+<?php include("footer.php") ?>
